de6121d2dc6ddf08b6fbf22c6973a49fd0d223be
Ewan Lockwood <ewan@plott.co.uk>
2025-09-16 15:32:50 +0100

    Initial Commit

---
+++ acf-live-preview.php
@@ -0,0 +1,13 @@
+/*
+ * Plugin Name:       ACF Flexible Content Live Preview
+ * Description:       Provides real-time preview for ACF Flexible Content fields.
+ * Version:           0.0.1
+ * Author:            Ewan Lockwood
+ * Author URI:        https://plott.co.uk
+ * Text Domain:       acf-live-preview
+ */
+
+// Prevent direct file access using ABSPATH constant
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
