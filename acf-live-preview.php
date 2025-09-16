3a42489eb163a4dad00d0b5548b482101190dc70
Ewan Lockwood <ewan@plott.co.uk>
2025-09-16 15:38:19 +0100

    Add opening php tag

--- acf-live-preview.php
+++ acf-live-preview.php
@@ -1,13 +1,14 @@
+<?php
 /*
- * Plugin Name:       ACF Flexible Content Live Preview
- * Description:       Provides real-time preview for ACF Flexible Content fields.
- * Version:           0.0.1
- * Author:            Ewan Lockwood
- * Author URI:        https://plott.co.uk
- * Text Domain:       acf-live-preview
- */
+* Plugin Name:       ACF Flexible Content Live Preview
+* Description:       Provides real-time preview for ACF Flexible Content fields.
+* Version:           0.0.1
+* Author:            Ewan Lockwood
+* Author URI:        https://plott.co.uk
+* Text Domain:       acf-live-preview
+*/
 
-// Prevent direct file access using ABSPATH constant
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+    // Prevent direct file access using ABSPATH constant
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit;
+    }
